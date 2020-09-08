550dcdd433a8737ba0c5222a027ef6c79e916ec8
ZeLarpMaster <guillaumejean46@gmail.com>
2020-09-08 01:08:07 -0400

    Added tests for shard_info

--- lib/mobius/core/shard_info.ex
+++ lib/mobius/core/shard_info.ex
@@ -12,12 +12,12 @@ defmodule Mobius.Core.ShardInfo do
   def new(fields), do: struct!(__MODULE__, fields)
 
   @spec from_count(pos_integer) :: [t()]
-  def from_count(count) do
+  def from_count(count) when is_integer(count) and count > 0 do
     for num <- 0..(count - 1) do
       new(number: num, count: count)
     end
   end
 
   @spec to_list(t()) :: [integer]
-  def to_list(info), do: [info.number, info.count]
+  def to_list(%__MODULE__{} = info), do: [info.number, info.count]
 end
