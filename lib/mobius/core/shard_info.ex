c02cf0ad150cd8a1e14314161ae50679ff827abb
ZeLarpMaster <guillaumejean46@gmail.com>
2020-09-08 01:58:26 -0400

    Using doctests for the shard info tests

--- lib/mobius/core/shard_info.ex
+++ lib/mobius/core/shard_info.ex
@@ -11,6 +11,16 @@ defmodule Mobius.Core.ShardInfo do
   @spec new(keyword) :: t()
   def new(fields), do: struct!(__MODULE__, fields)
 
+  @doc """
+  Generate a list of ShardInfo structs going from 0 to n-1
+
+      iex> from_count(0)
+      ** (FunctionClauseError) no function clause matching in Mobius.Core.ShardInfo.from_count/1
+      iex> from_count(1) == [new(number: 0, count: 1)]
+      true
+      iex> from_count(2) == [new(number: 0, count: 2), new(number: 1, count: 2)]
+      true
+  """
   @spec from_count(pos_integer) :: [t()]
   def from_count(count) when is_integer(count) and count > 0 do
     for num <- 0..(count - 1) do
@@ -18,6 +28,12 @@ defmodule Mobius.Core.ShardInfo do
     end
   end
 
+  @doc """
+  Convert a ShardInfo struct into a list
+
+      iex> to_list(new(number: 3, count: 10))
+      [3, 10]
+  """
   @spec to_list(t()) :: [integer]
   def to_list(%__MODULE__{} = info), do: [info.number, info.count]
 end
--- test/mobius/core/shard_info_test.exs
+++ test/mobius/core/shard_info_test.exs
@@ -1,16 +1,4 @@
 defmodule Mobius.Core.ShardInfoTest do
   use ExUnit.Case, async: true
-
-  import Mobius.Core.ShardInfo
-
-  test "from_count/1 generates a list of shard infos" do
-    assert from_count(1) == [new(number: 0, count: 1)]
-
-    list = from_count(3)
-    assert list == [new(number: 0, count: 3), new(number: 1, count: 3), new(number: 2, count: 3)]
-  end
-
-  test "to_list/1 returns a list with the number and count" do
-    assert to_list(new(number: 21, count: 42)) == [21, 42]
-  end
+  doctest Mobius.Core.ShardInfo, import: true
 end
