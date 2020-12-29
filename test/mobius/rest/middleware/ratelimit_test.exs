2f1dc6dc5a2d9b80251302eeedc1708beb9ad6bb
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-29 16:31:12 -0500

    refactor: Renamed the helper functions in the ratelimit middleware test

--- test/mobius/rest/middleware/ratelimit_test.exs
+++ test/mobius/rest/middleware/ratelimit_test.exs
@@ -10,7 +10,7 @@ defmodule Mobius.Rest.Middleware.RatelimitTest do
 
   test "doesn't wait if route still has remaining calls", ctx do
     url = "https://discord.com/api/v6/something"
-    mock(fn %{url: ^url} -> response(2, 50, 0) end)
+    mock(fn %{url: ^url} -> response_with_route_ratelimit(remaining: 2, delay_ms: 50) end)
     Tesla.get(ctx.client, url)
 
     assert_get_time(ctx.client, url, 0)
@@ -18,7 +18,7 @@ defmodule Mobius.Rest.Middleware.RatelimitTest do
 
   test "waits until reset-after if route is exhausted", ctx do
     url = "https://discord.com/api/v6/something"
-    mock(fn %{url: ^url} -> response(0, 50, 0) end)
+    mock(fn %{url: ^url} -> response_with_route_ratelimit(remaining: 0, delay_ms: 50) end)
     Tesla.get(ctx.client, url)
 
     assert_get_time(ctx.client, url, 50)
@@ -26,7 +26,7 @@ defmodule Mobius.Rest.Middleware.RatelimitTest do
 
   test "waits until reset-after if global ratelimit was exceeded", ctx do
     url = "https://discord.com/api/v6/something"
-    mock(fn %{url: ^url} -> response(0, 100, 50) end)
+    mock(fn %{url: ^url} -> response_with_global_ratelimit(50) end)
     Tesla.get(ctx.client, url)
 
     assert_get_time(ctx.client, url, 50)
@@ -38,7 +38,7 @@ defmodule Mobius.Rest.Middleware.RatelimitTest do
 
     mock(fn %{method: :get} ->
       [{:remaining, remaining}] = :ets.lookup(table, :remaining)
-      response(remaining, 50, 0)
+      response_with_route_ratelimit(remaining: remaining, delay_ms: 50)
     end)
 
     # First 2 calls to associate both routes to the same bucket
@@ -60,24 +60,27 @@ defmodule Mobius.Rest.Middleware.RatelimitTest do
     |> assert_in_delta(expected_time, 10)
   end
 
-  defp response(remaining, delay, global_delay) do
-    cond do
-      global_delay > 0 ->
-        {429,
-         [
-           {"retry-after", "#{global_delay}"},
-           {"x-ratelimit-global", "true"}
-         ], %{"retry_after" => global_delay, "global" => true}}
+  defp response_with_global_ratelimit(global_delay) do
+    headers = [
+      {"retry-after", "#{global_delay}"},
+      {"x-ratelimit-global", "true"}
+    ]
 
-      remaining < 0 ->
-        {429, headers(0, delay), %{"retry_after" => delay, "global" => false}}
+    {429, headers, %{"retry_after" => global_delay, "global" => true}}
+  end
 
-      true ->
-        {204, headers(remaining, delay), nil}
+  defp response_with_route_ratelimit(opts) do
+    remaining = Keyword.fetch!(opts, :remaining)
+    delay = Keyword.fetch!(opts, :delay_ms)
+
+    if remaining < 0 do
+      {429, make_ratelimit_headers(0, delay), %{"retry_after" => delay, "global" => false}}
+    else
+      {204, make_ratelimit_headers(remaining, delay), nil}
     end
   end
 
-  defp headers(remaining, delay) do
+  defp make_ratelimit_headers(remaining, delay) do
     [
       {"retry-after", "#{delay}"},
       {"x-ratelimit-limit", "5"},
