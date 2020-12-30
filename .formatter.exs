1a4d43ca97c7fb3ef347d9f1055d48880a6e205a
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-12-30 17:33:16 -0500

    Format listen macro without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,12 @@
 # Used by "mix format"
+locals_without_parens = [
+  # Mobius.Services.Cog
+  listen: 2,
+  listen: 3
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
