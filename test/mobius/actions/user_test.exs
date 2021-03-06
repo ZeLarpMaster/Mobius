2456a7e1d99bc23dec4b7f1a3d1cefb86afa539d
ZeLarpMaster <guillaumejean46@gmail.com>
2021-03-05 21:21:01 -0500

    test: Added tests for user actions

---
+++ test/mobius/actions/user_test.exs
@@ -0,0 +1,26 @@
+defmodule Mobius.Actions.UserTest do
+  use ExUnit.Case
+
+  import Mobius.Fixtures
+  import Mobius.Generators
+
+  alias Mobius.Actions.User
+
+  setup :reset_services
+  setup :stub_socket
+  setup :get_shard
+  setup :handshake_shard
+
+  describe "get_cached_user/1" do
+    test "returns nil if not cached" do
+      User.get_cached_user(random_snowflake())
+    end
+
+    test "returns the user if cached" do
+      cached = user()
+      send_payload(op: :dispatch, type: "USER_UPDATE", data: cached)
+
+      assert cached == User.get_cached_user(cached["id"])
+    end
+  end
+end
