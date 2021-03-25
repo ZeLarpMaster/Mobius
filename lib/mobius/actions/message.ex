ae4dd069ca683d64f8aefeecda1f5ee92ee6d5ef
ZeLarpMaster <guillaumejean46@gmail.com>
2021-03-24 22:12:41 -0400

    feat(actions): Implementing file sending

--- lib/mobius/actions/message.ex
+++ lib/mobius/actions/message.ex
@@ -8,29 +8,47 @@ defmodule Mobius.Actions.Message do
   alias Mobius.Rest.Client
   alias Mobius.Services.Bot
 
+  @type file :: Rest.Message.file()
   @type message_body :: Rest.Message.message_body()
 
   @doc """
   Send a message in a channel
 
-  The params can be:
+  The overall limit of the request is 8MB which includes files sent.
+  Keep this in mind when sending potentially large messages.
+
+  The params for the body of the message can be:
     - `content`: The message's content (maximum 2000 chars)
     - `nonce`: random string or integer to compare with received messages and check its reception
     - `tts`: true if this message should be read with tts
+    - `file`: a `{content, filename}` tuple (see File Uploads below for details)
     - `embed`: an embed with type `rich` and some restrictions (see Discord docs)
     - `allowed_mentions`: the mentions allowed in this message (see Discord docs)
     - `message_reference`: identifies the message being replied to (see Discord docs)
 
+  You must provide at least one of `content`, `embed` or `file`.
+
+  ## File Uploads
+
+  The `file` param in the body is the tuple `{content, filename}` where
+  `content` is `t:iodata/0` or a `Stream` of `t:binary/0` and `filename` is a string
+
+  You can use an uploaded image inside the same message's embed by using
+  `"attachment://" <> filename` as the image url in the embed.
+  This only works if the filename has a proper file extension (such as ".png", ".jpg", etc.)
+
   ## Example
 
-      iex> send_message([content: "Some content"], channel_id)
+      iex> send_message(%{content: "Some content"}, channel_id)
       {:ok, %Mobius.Models.Message{} = message}
 
   ## Documentation
 
   Refer to the documentation for details on the limitations given by Discord.
-  This function is specifically for `content-type: application/json`.
-  See `send_file/3` for `content-type: multipart/form-data`.
+  This function handles both `content-type: application/json` and
+  `content-type: multipart/form-data` (which is used when also sending a file).
+  The implementation deals with how to send the parameters
+  depending on which content type is used.
 
   Relevant documentation:
   https://discord.com/developers/docs/resources/channel#create-message
@@ -38,8 +56,8 @@ defmodule Mobius.Actions.Message do
   @spec send_message(message_body(), Snowflake.t()) :: Client.result(Message.t())
   def send_message(body, channel_id) do
     cond do
-      not Map.has_key?(body, :content) and not Map.has_key?(body, :embed) ->
-        {:error, "Must have at least one of content or embed when sending a message"}
+      Enum.any?([:content, :embed, :file], &Map.has_key?(body, &1)) ->
+        {:error, "Must have at least one of content, embed or file when sending a message"}
 
       String.length(Map.get(body, :content, "")) > 2000 ->
         {:error, "Content is too long (maximum 2000 characters)"}
--- lib/mobius/rest/message.ex
+++ lib/mobius/rest/message.ex
@@ -43,64 +43,52 @@ defmodule Mobius.Rest.Message do
           optional(:fail_if_not_exists) => boolean
         }
 
-  @typedoc "Both `:content` and `:embed` are optional, but at least one of the two must be given"
   @type message_body :: %{
           optional(:content) => String.t(),
           optional(:nonce) => String.t(),
           optional(:tts) => boolean,
           optional(:embed) => embed(),
+          optional(:file) => {Multipart.part_value(), String.t()},
           optional(:allowed_mentions) => allowed_mentions(),
           optional(:message_reference) => message_reference()
         }
 
   @doc """
-  Does the API call for "Create Message" with `content-type: application/json`
+  Does the API call for "Create Message" and handles both `content-type`s as needed
 
-  This doesn't allow sending files since allowing both in the same function would make it very
-  complicated with little added value. Instead sending files is implemented in `send_file/5`.
-  """
-  @spec send_message(Client.client(), Snowflake.t(), message_body()) :: Client.result(Message.t())
-  def send_message(client, channel_id, body) do
-    client
-    |> Tesla.post("/channels/:channel_id/messages", body,
-      opts: [path_params: [channel_id: channel_id]]
-    )
-    |> Client.parse_response(&Message.parse/1)
-  end
+  When sending a file, message params will be sent in `payload_json` as a json string.
 
-  @doc """
-  Does the API call for "Create Message" with `content-type: multipart/form-data`
+  The file content can be `t:iodata/0` or a `Stream` of `t:binary/0`.
+  If you want to send a file by path, use `File.stream!(path, [:read], 2048)` to get its content.
 
-  This function takes iodata or a `Stream` of data for file content.
-  If you want to send a file by path, use `File.stream!(path, [:read], 2048)`
-  to get the file contents.
-
-  If you want to use the uploaded image in your embeds,
+  If you want to use an uploaded image in your embeds,
   you can do so by using `"attachment://" <> filename` as the url.
   This only works if the filename has a proper image extension (such as `.png`, `.jpg`, etc.)
-
-  This always sends a file, if you don't want to send a file, use `send_message/3` instead.
   """
-  @spec send_file(
-          Client.client(),
-          Snowflake.t(),
-          Multipart.part_value(),
-          String.t(),
-          message_body()
-        ) :: Client.result(Message.t())
-  def send_file(client, channel_id, file_content, filename, body) do
+  @spec send_message(Client.client(), Snowflake.t(), message_body()) :: Client.result(Message.t())
+  def send_message(client, channel_id, %{file: {file, filename}} = body) do
+    body = Map.drop(body, [:file])
+
     multipart =
       Multipart.new()
       |> Multipart.add_field("payload_json", Jason.encode!(body),
         headers: [{"content-type", "application/json"}]
       )
-      |> Multipart.add_file_content(file_content, filename,
+      |> Multipart.add_file_content(file, filename,
         name: "file",
         detect_content_type: true
       )
 
     client
     |> Tesla.post("/channels/:channel_id/messages", multipart,
+      opts: [path_params: [channel_id: channel_id]]
+    )
+    |> Client.parse_response(&Message.parse/1)
+  end
+
+  def send_message(client, channel_id, body) do
+    client
+    |> Tesla.post("/channels/:channel_id/messages", body,
       opts: [path_params: [channel_id: channel_id]]
     )
     |> Client.parse_response(&Message.parse/1)
