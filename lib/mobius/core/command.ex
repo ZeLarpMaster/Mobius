07da0e5114754faaeaa3afb577b88432956d6c11
ZeLarpMaster <guillaumejean46@gmail.com>
2021-04-14 22:05:55 -0400

    refactor: Command execution (#83)

--- lib/mobius/core/command.ex
+++ lib/mobius/core/command.ex
@@ -13,11 +13,17 @@ defmodule Mobius.Core.Command do
           handler: function()
         }
 
+  @type processed :: %{
+          String.t() => %{
+            non_neg_integer() => [t()]
+          }
+        }
+
   @type handle_message_result ::
           {:ok, any()}
           | :not_a_command
-          | {:too_few_args, t(), non_neg_integer()}
-          | {:invalid_args, [{{atom(), ArgumentParser.arg_type()}, String.t()}]}
+          | {:too_few_args, [non_neg_integer()], non_neg_integer()}
+          | {:invalid_args, [t]}
 
   @spec command_handler_name(String.t()) :: atom()
   def command_handler_name(command_name) do
@@ -34,47 +40,41 @@ defmodule Mobius.Core.Command do
   @spec arg_count(t()) :: non_neg_integer()
   def arg_count(%__MODULE__{} = command), do: length(command.args)
 
-  @spec execute_command([t()], String.t(), Message.t()) :: handle_message_result()
+  @spec execute_command(processed(), String.t(), Message.t()) :: handle_message_result()
   def execute_command(commands, prefix, %Message{content: content} = message) do
-    arg_values = split_arguments(content)
-
-    Enum.reduce_while(commands, :not_a_command, fn %__MODULE__{} = command, acc ->
-      new =
-        cond do
-          not String.starts_with?(content, prefix <> command.name) -> :not_a_command
-          arg_count(command) != length(arg_values) -> {:too_few_args, command, length(arg_values)}
-          true -> try_command(command, arg_values, message)
-        end
-
-      decide_by_priority(acc, new)
-    end)
-  end
-
-  defp decide_by_priority(old, new) do
-    # Overwrite the accumulator depending on a priority list:
-    # :ok (which halts the reduce) > :invalid_args > :too_few_args > :not_a_command
-    # All the :cont are to let other clauses have their chance
-    # However if one of the clauses has the right amount of args, but wrong types, that's the
-    #   error we want to return ultimately if no other clause matches those arguments
-    # Same logic applies for :too_few_args, if one clause is the right command with the wrong
-    #   amount of args, but no other clause matches the command, that's the error we want
-    # Finally, if none of the commands matches, we return :not_a_command
-    case {old, new} do
-      {{:ok, _} = value, _} -> {:halt, value}
-      {_, {:ok, _} = value} -> {:halt, value}
-      {{:invalid_args, _} = value, _} -> {:cont, value}
-      {_, {:invalid_args, _} = value} -> {:cont, value}
-      {{:too_few_args, _, _} = value, _} -> {:cont, value}
-      {_, {:too_few_args, _, _} = value} -> {:cont, value}
-      {:not_a_command, _} -> {:cont, :not_a_command}
-      {_, :not_a_command} -> {:cont, :not_a_command}
-    end
-  end
-
-  defp try_command(command, arg_values, %Message{} = message) do
-    with {:ok, values} <- parse_arg_values(command, arg_values) do
+    with {:ok, content} <- match_prefix(prefix, content),
+         {:ok, name, arg_values} <- split_arguments(content),
+         {:ok, groups} <- get_command(commands, name),
+         {:ok, clauses} <- get_clauses(groups, length(arg_values)),
+         {:ok, command, values} <- find_clause(clauses, arg_values) do
       {:ok, apply(command.handler, [message | values])}
     end
+  end
+
+  @spec preprocess_commands([t()]) :: processed()
+  def preprocess_commands(commands) do
+    commands
+    |> Enum.reverse()
+    |> Enum.group_by(fn %__MODULE__{name: name} -> name end)
+    |> Map.new(fn {name, commands} -> {name, Enum.group_by(commands, &arg_count/1)} end)
+  end
+
+  defp get_command(commands, name) do
+    case Map.fetch(commands, name) do
+      :error -> :not_a_command
+      {:ok, clause_groups} -> {:ok, clause_groups}
+    end
+  end
+
+  defp get_clauses(clause_groups, arity) do
+    case Map.fetch(clause_groups, arity) do
+      :error -> {:too_few_args, Map.keys(clause_groups), arity}
+      {:ok, clauses} -> {:ok, clauses}
+    end
+  end
+
+  defp find_clause(clauses, arg_values) do
+    Enum.find_value(clauses, {:invalid_args, clauses}, &parse_arg_values(&1, arg_values))
   end
 
   defp parse_arg_values(%__MODULE__{} = command, values) do
@@ -86,16 +86,23 @@ defmodule Mobius.Core.Command do
       end)
       |> Enum.split_with(fn {_, _, parse_result} -> parse_result == :error end)
 
-    if errors != [] do
-      {:invalid_args, Enum.map(errors, fn {arg, value, _} -> {arg, value} end)}
-    else
-      {:ok, Enum.map(valids, fn {{_name, type}, _, val} -> {type, val} end)}
+    if errors == [] do
+      {:ok, command, Enum.map(valids, fn {{_name, type}, _, val} -> {type, val} end)}
     end
   end
 
-  defp split_arguments(message) do
-    message
-    |> String.split()
-    |> tl()
+  defp match_prefix(prefix, content) do
+    if String.starts_with?(content, prefix) do
+      {:ok, String.replace_prefix(content, prefix, "")}
+    else
+      :not_a_command
+    end
+  end
+
+  defp split_arguments(content) do
+    case String.split(content) do
+      [] -> :not_a_command
+      [name | args] -> {:ok, name, args}
+    end
   end
 end
